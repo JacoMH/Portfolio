4ed9521a68f4999c368c14fd9e77b5c84847d8bb
JacoMH <jacob.heyes@student.shu.ac.uk>
2024-06-01 10:50:01 +0100

    tailwind added

---
+++ content/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="../css/output.css" rel="stylesheet">
+</head>
+<body>
+    <div name='welcome' class='flex flex-col align-top justify-center'>
+        <div class='text-3xl font-bold underline text-center'>
+            Jacob Heyes
+        </div>
+        <button class='bg-green-400 px-4 py-2 text-current rounded-full text-9xl'>Enter Portfolio</button>
+    </div>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/content/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      spacing: {
+        'xxs': '10rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
